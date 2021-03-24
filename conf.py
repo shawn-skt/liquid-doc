447fc00adc8d1b584eea994e0a644795ee94c648
vita-dounai <lichenxi.webank@gmail.com>
2021-03-24 12:43:55 +0800

    change version number

--- conf.py
+++ conf.py
@@ -24,7 +24,7 @@ copyright = '2021, All rights reserved'
 author = 'Chenxi Li'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.0.0-rc1'
 
 
 # -- General configuration ---------------------------------------------------
